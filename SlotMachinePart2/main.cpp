3110aba33572a4fa925fc954bfc56807200609b7
Aniket Sedhai <asedhai@masonite.com>
2024-07-04 23:57:15 -0500

    Initial Commit

---
+++ SlotMachinePart2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SlotMachinePart2
+//
+//  Created by Aniket Sedhai on 7/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
